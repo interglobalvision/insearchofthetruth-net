c21f3760e8494ae2f4c4ac97e8d3478e6c5d4c0e
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2017-07-30 18:44:43 -0500

    scaffold index

--- index.php
+++ index.php
@@ -5,19 +5,37 @@ get_header();
 <main id="main-content">
   <section id="posts">
     <div class="container">
-      <div class="grid-row">
 
 <?php
 if( have_posts() ) {
   while( have_posts() ) {
     the_post();
+
+    if (has_category()) {
+      $category = get_the_category();
+      $cat_name = $category[0]->cat_name;
+      $cat_id = get_cat_ID( $name );
+      $cat_link = get_category_link( $cat_id );
+    }
 ?>
 
-        <article <?php post_class('grid-item item-s-12'); ?> id="post-<?php the_ID(); ?>">
+        <article <?php post_class('grid-row'); ?> id="post-<?php the_ID(); ?>">
 
-          <a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
+          <div class="grid-item item-s-12 item-m-4">
+            <h1><a class="link-underline" href="<?php the_permalink() ?>"><?php the_title(); ?></a><h1>
+          </div>
 
-          <?php the_content(); ?>
+          <div class="grid-item item-s-12 item-m-4">
+            <time datetime="<?php the_time('Y-m-d'); ?>"><?php echo get_the_date(); ?></time><br>
+
+            <?php if (has_category() && $cat_name !== 'Uncategorized') { ?>
+            <a href="<?php echo esc_url($cat_link); ?>"><?php echo $cat_name; ?></a>
+            <?php } ?>
+          </div>
+
+          <div class="grid-item item-s-12 item-m-4">
+            <?php the_post_thumbnail('item-l-4'); ?>
+          </div>
 
         </article>
 
@@ -29,7 +47,6 @@ if( have_posts() ) {
 <?php
 } ?>
 
-      </div>
     </div>
   </section>
 
