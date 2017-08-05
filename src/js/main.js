2bd4c223f8c0444d423ad19cc9a286e2c42f9a7e
cas <cas@interglobal.vision>
2017-08-04 22:07:22 -0500

    add basic filter functionality

--- src/js/main.js
+++ src/js/main.js
@@ -24,6 +24,11 @@ Site = {
         slideToClickedSlide: true,
       });
 
+      // Init grid
+      if ($('#portraits').length) {
+        Site.Portraits.init();
+      }
+
       if ($('.paypal-form-holder').length) {
         Site.Paypal.init();
       }
@@ -51,7 +56,7 @@ Site.Paypal = {
   init: function() {
     var _this = this;
 
-    var $form = $('.paypal-form-holder form')
+    var $form = $('.paypal-form-holder form');
 
     $form.each(function() {
       _this.styleForm($(this));
@@ -80,6 +85,77 @@ Site.Paypal = {
       $form.find('table').remove();
       $select.wrap('<div class="grid-item item-s-6"></div>');
     }
+  },
+
+};
+
+Site.Portraits = {
+  init: function() {
+    var _this = this;
+
+    _this.$form = $('#portraits-filters');
+    _this.$filters = _this.$form.find('select');
+
+    _this.initGrid();
+
+    // Bind select filters
+    _this.$filters = $('.filter-select').on('change', _this.handleFilterChange.bind(_this));
+
+  },
+
+  initGrid: function() {
+    var _this = this;
+
+    // Set grid with isotope
+    _this.$grid = $('#portraits-grid').isotope({
+      // options
+      itemSelector: '.grid-item',
+      layoutMode: 'fitRows'
+    });
+
+
+    // Workaround to make it compatible with lazysizes
+    _this.$grid[0].addEventListener('load', (function(){
+      var runs;
+      var update = function(){
+        _this.$grid.isotope('layout');
+        runs = false;
+      };
+      return function(){
+        if(!runs){
+          runs = true;
+          setTimeout(update, 33);
+        }
+      };
+    }()), true);
+
+  },
+
+  handleFilterChange: function(event) {
+    var _this = this;
+
+    var filterSelector = _this.getFilterSelector();
+
+    console.log('filter',filterSelector);
+
+    _this.$grid.isotope({
+      filter: filterSelector,
+    });
+  },
+
+  getFilterSelector: function() {
+    var _this = this;
+
+    var selector = '';
+
+    _this.$filters.each( function(index) {
+      if(this.value) {
+        selector += '[data-' + this.dataset.filter + '=' + this.value + ']';
+      }
+    });
+
+    return selector
+
   },
 
 };
