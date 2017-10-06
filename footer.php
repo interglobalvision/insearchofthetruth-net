a09da58712c6255952ba18da1eebb6c1a880e799
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2017-10-06 16:57:34 -0500

    add portraits to footer

--- footer.php
+++ footer.php
@@ -1,5 +1,36 @@
-  <footer id="footer">
+<?php
+$args = array(
+  'post_type' => 'portrait',
+  'orderby'   => 'rand',
+  'posts_per_page'  => 6,
+);
+
+$query = new WP_Query( $args );
+
+if ($query->have_posts() && !is_front_page()) {
+?>
+  <footer id="footer" class="container">
+    <div class="grid-row justify-center align-items-end">
+<?php
+  while ($query->have_posts()) {
+    $query->the_post();
+
+    if (has_post_thumbnail()) {
+?>
+      <div class="item-s-3 item-m-2">
+        <a href="<?php echo home_url(); ?>">
+          <?php the_post_thumbnail('item-l-3', 'class=u-block'); ?>
+        </a>
+      </div>
+<?php
+    }
+  }
+?>
+    </div>
   </footer>
+<?php
+}
+?>
 
 </section>
 
