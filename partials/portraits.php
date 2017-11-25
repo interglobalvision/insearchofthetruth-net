b44a77de41d30c9293c9cf42099e3b08b0a4df9d
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2017-11-25 12:56:05 -0600

    update portrait thumb image size

--- partials/portraits.php
+++ partials/portraits.php
@@ -54,7 +54,7 @@ if ( $portraits->have_posts() ) {
       render_filter_select($subjects, 'subject', $item_classes);
     }
   ?>
-  
+
         </form>
           <a href="#" class="toggle-map grid-item font-bold">Map <?php echo url_get_contents(get_template_directory_uri() . '/dist/img/arrow-right.svg'); ?></a>
         </div>
@@ -72,7 +72,7 @@ if ( $portraits->have_posts() ) {
         $filters_data = get_post_filters_data($post);
   ?>
           <article <?php post_class('portrait margin-bottom-small u-pointer'); ?> id="post-<?php the_ID(); ?>" data-groups='<?php echo json_encode($filters_data); ?>' data-youtube-id="<?php echo $youtube_id; ?>">
-          <a href="#!/portrait/<?php echo $youtube_id; ?>"><?php the_post_thumbnail('item-l-4'); ?></a>
+          <a href="#!/portrait/<?php echo $youtube_id; ?>"><?php the_post_thumbnail('portrait-16-9'); ?></a>
           </article>
   <?php
       }
